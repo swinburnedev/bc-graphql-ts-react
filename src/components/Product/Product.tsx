a2e1a293a5d119e8b5c59a0822e2a21d696520a7
Andy Swinburne <andy.swinburne@purplebricks.com>
2022-07-27 08:57:43 +0100

    Format price

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ProductByIdQuery } from '../../generated/graphql';
+import { Money, ProductByIdQuery } from '../../generated/graphql';
 import './styles.scss';
 
 interface Props {
@@ -10,33 +10,37 @@ const className = 'Product';
 
 const Product: React.FC<Props> = ({ data }) => {
     const {site: { product }} = data || {};
-  if (!product) {
-    return <div>No product available</div>;
-  }
+    if (!product) {
+        return <div>No product available</div>;
+    }
 
-  return (
-    <div className={className}>
-        <h1 className={`${className}__title`}>
-            {product.name}
-        </h1>
-        <p className={`${className}__description`}>{product.plainTextDescription}</p>
-        <p>{product.prices?.price.value}</p>
-        {!!product.images && !!product.images.edges && (
-            <div className={`${className}__image-list`}>
-            {product.images.edges.map((image, i) =>
-                image ? (
-                <img
-                    src={image.node.url1280wide}
-                    className={`${className}__image`}
-                    key={image.node.url1280wide}
-                    alt="TODO"
-                />
-                ) : null,
+    const formatLocalizedPrice = (price: Money | undefined) => {
+        return new Intl.NumberFormat(navigator.language, {style: 'currency', currency: price?.currencyCode}).format(price?.value);
+    }
+
+    return (
+        <div className={className}>
+            <h1 className={`${className}__title`}>
+                {product.name}
+            </h1>
+            <p className={`${className}__description`}>{product.plainTextDescription}</p>
+            <p>{formatLocalizedPrice(product.prices?.price)}</p>
+            {!!product.images && !!product.images.edges && (
+                <div className={`${className}__image-list`}>
+                {product.images.edges.map((image, i) =>
+                    image ? (
+                    <img
+                        src={image.node.url1280wide}
+                        className={`${className}__image`}
+                        key={image.node.url1280wide}
+                        alt="TODO"
+                    />
+                    ) : null,
+                )}
+                </div>
             )}
-            </div>
-        )}
         </div>
-  );
+    );
 };
 
 export default Product;
